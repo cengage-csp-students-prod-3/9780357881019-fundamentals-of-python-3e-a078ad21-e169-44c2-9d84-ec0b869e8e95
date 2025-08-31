c27135e02ce078e4fb726a4faf06781c2a9beb4d
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-08-31 03:20:03 +0000

    Testing Sun Aug 31 2025 03:20:03 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex05/student/cuboid.py
+++ chapter1/ex05/student/cuboid.py
@@ -1,1 +1,6 @@
 # Write your program here
+height= float(input("Enter teh height:"))
+width= float (input("Enter the width:"))
+depth= float (input("Enter the depth:"))
+cuboid= height* width * depth
+print("The volume is" , cuboid , "cubic units.")
