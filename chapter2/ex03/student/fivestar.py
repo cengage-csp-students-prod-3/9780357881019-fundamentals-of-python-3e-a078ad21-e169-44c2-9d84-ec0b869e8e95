fdeba259aff2ecaca813c8fc2e1f48c91b52966b
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-09-09 01:36:28 +0000

    Testing Tue Sep 09 2025 01:36:28 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/fivestar.py
+++ chapter2/ex03/student/fivestar.py
@@ -1,32 +1,37 @@
 """
 Program: fivestar.py
 Author: Ziynet Akca
-This program calculates the total charge for a customer`s video rentals.
-The program should prompt the user for the number of each type of video and output the total cost.
+
+This program calculates the total charge for a customer’s video rentals.
+It prompts the user for the number of new videos and oldies rented and
+computes the total cost based on fixed rates.
 
 Analysis:
-The user provides the number of new videos and the number of old videos.
-The program uses the formula==>
+- User inputs:
+    - Number of new videos
+    - Number of oldies
+- Prices:
+    - New videos: $3.00 per night
+    - Oldies: $2.00 per night
+- Output: Total cost of the rentals
 
-newVideoCost= numOfNewVideo * 3.00
+Design (pseudocode):
+1. Prompt the user to enter the number of new videos
+2. Prompt the user to enter the number of oldies
+3. Compute newVideoCost = number of new videos * 3.00
+4. Compute oldVideoCost = number of oldies * 2.00
+5. Compute totalCost = newVideoCost + oldVideoCost
+6. Display totalCost formatted exactly as "The total cost is $<amount>"
+"""
+
+# Request the number of videos
+numOfNewVideo = int(input("Enter the number of new videos: "))
+numOfOldVideo = int(input("Enter the number of oldies: "))
+
+# Compute costs
+newVideoCost = numOfNewVideo * 3.00
 oldVideoCost = numOfOldVideo * 2.00
 totalCost = newVideoCost + oldVideoCost
 
-Design:
-Design:
-1. Prompt the user to enter number of new videos and old videos.
-2. Calculate cost of new videos: newVideoCost = numOfNewVideo * newVideoCostDaily
-3. Calculate cost of old videos: oldVideoCost = numOfOldVideo * oldVideoCostDaily
-4. Compute totalCost = newVideoCost + oldVideoCost
-5. Display totalCost
-
-
-"""
-numOfNewVideo = int(input("The number of new videos"))
-numOfOldVideo = int (input("The number of old videos"))
-newVideoCostDaily = int(input("Enter the cost of new video daily"))
-oldVideoCostDaily = int(input("Enter the cost of old video daily"))
-newVideoCost= numOfNewVideo * newVideoCostDaily
-oldVideoCost = numOfOldVideo * oldVideoCostDaily
-totalCost = newVideoCost + oldVideoCost
-print("The total cost is " , str(totalCost))
+# Display total cost in the exact format expected
+print("The total cost is $" + str(totalCost))
