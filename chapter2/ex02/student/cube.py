93c525d69aae9073ce639d05b43639c1927b4bc6
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-09-09 00:30:33 +0000

    Testing Tue Sep 09 2025 00:30:33 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/cube.py
+++ chapter2/ex02/student/cube.py
@@ -2,6 +2,6 @@
 #Programmer: Ziynet Akca
 #This code calculates the surface area of a cube by using math module
 
-edge = float(5)
+edge = float(input("Enter the cube's edge: "))
 surfaceArea = 6 * edge**2
-print ("The surface area is " , surfaceArea , "square units.")
+print("The surface area is", surfaceArea, "square units.")
