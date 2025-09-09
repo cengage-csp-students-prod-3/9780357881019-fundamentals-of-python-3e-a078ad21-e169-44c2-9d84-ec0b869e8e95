f1de8080ed310bfe50b322c7705190073947e80a
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-09-09 00:55:48 +0000

    Testing Tue Sep 09 2025 00:55:48 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/cube.py
+++ chapter2/ex02/student/cube.py
@@ -18,14 +18,9 @@ Design (pseudocode):
 """
 
 
-
-
-# Step 1: Prompt for input
-edge = float(input("Enter the cube's edge: "))
-
-# Step 2: Compute surface area
+edge = 5.0
 surfaceArea = 6 * edge**2
-
-# Step 3: Display the result
 print("The surface area is", surfaceArea, "square units.")
+
+
 
