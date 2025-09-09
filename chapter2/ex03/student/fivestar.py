9862989f02b7fd5a236c5dbcdfb3170af7fcd294
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-09-09 01:29:19 +0000

    Testing Tue Sep 09 2025 01:29:19 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/fivestar.py
+++ chapter2/ex03/student/fivestar.py
@@ -1,1 +1,30 @@
-# Write your program here
+"""
+Program: fivestar.py
+Author: Ziynet Akca
+This program calculates the total charge for a customer`s video rentals.
+The program should prompt the user for the number of each type of video and output the total cost.
+
+Analysis:
+The user provides the number of new videos and the number of old videos.
+The program uses the formula==>
+
+newVideoCost= numOfNewVideo * 3.00
+oldVideoCost = numOfOldVideo * 2.00
+totalCost = newVideoCost + oldVideoCost
+
+Design:
+Design:
+1. Prompt the user to enter number of new videos and old videos.
+2. Calculate cost of new videos: newVideoCost = numOfNewVideo * 3.00
+3. Calculate cost of old videos: oldVideoCost = numOfOldVideo * 2.00
+4. Compute totalCost = newVideoCost + oldVideoCost
+5. Display totalCost
+
+
+"""
+numOfNewVideo = int(input("The number of new videos"))
+numOfOldVideo = int (input("The number of old videos"))
+newVideoCost= numOfNewVideo * 3.00
+oldVideoCost = numOfOldVideo * 2.00
+totalCost = newVideoCost + oldVideoCost
+print("The total cost is " , str(totalCost))
