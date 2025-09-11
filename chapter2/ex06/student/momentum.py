6f7a0bc17b686c58d40cd5d8142be619b9987a90
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-09-11 01:59:22 +0000

    Testing Thu Sep 11 2025 01:59:22 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/momentum.py
+++ chapter2/ex06/student/momentum.py
@@ -1,1 +1,27 @@
-# Write your program here
+"""
+Program: momentum.py
+Author: Ziynet Akca
+
+This program calculates the object`s kinetic energy.
+
+ANALYSIS
+
+The user provides 2 inputs(mass and velocity)
+The program computes the object`s kinetic energy by using the formula KE=(1/2)mv**2
+
+DESIGN
+1. Prompt from the user mass amount
+2.Prompt from the user velocity amount
+3.Calculate momentum by using formula Momentum = mass * velocity
+4.Calculate the object`s kinetic energy by using formula KE=(1/2) mv**2
+5.Display the result
+
+"""
+mass = float(input ("Enter the object`s mass: "))
+velocity = float (input("Enter the object`s velocity: "))
+momentum= mass*velocity
+print("The object`s momentum is ", str(momentum))
+
+KE= 1/2 * (mass*velocity**2)
+
+print("The object`s kinetic energy is ", str(KE))
