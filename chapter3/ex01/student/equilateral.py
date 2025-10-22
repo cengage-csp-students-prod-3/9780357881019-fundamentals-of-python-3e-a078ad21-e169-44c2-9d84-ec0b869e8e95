fb3d470d9aaf920a183b736e6745e8557f6efb80
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-22 00:58:48 +0000

    Testing Wed Oct 22 2025 00:58:48 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/equilateral.py
+++ chapter3/ex01/student/equilateral.py
@@ -1,1 +1,9 @@
-# Write your program here
+
+side1 = float(input("Enter the first side: "))
+side2 = float(input("Enter the second side: "))
+side3 = float(input("Enter the third side: "))
+
+if side1 == side2 == side3:
+    print("The triangle is equilateral.")
+else:
+    print("The triangle is not equilateral.")
