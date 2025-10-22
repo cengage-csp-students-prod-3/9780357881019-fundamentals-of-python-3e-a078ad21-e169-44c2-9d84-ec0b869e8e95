a11a19f084cba7ac6b9702a940373400434f2eb2
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-22 01:54:17 +0000

    Testing Wed Oct 22 2025 01:54:17 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex05/student/population.py
+++ chapter3/ex05/student/population.py
@@ -1,1 +1,11 @@
-# Write your program here
+# population.py
+
+initial = int(input("Enter the initial number of organisms: "))
+rate = float(input("Enter the rate of growth [a real number > 1]: "))
+hours_to_rate = float(input("Enter the number of hours to achieve the rate of growth: "))
+total_hours = float(input("Enter the total hours of growth: "))
+
+periods = total_hours / hours_to_rate
+population = initial * (rate ** periods)
+
+print("The total population is", int(population))
