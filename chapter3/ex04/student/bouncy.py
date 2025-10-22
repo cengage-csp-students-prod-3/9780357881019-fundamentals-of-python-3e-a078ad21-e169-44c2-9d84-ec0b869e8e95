168695fb2eb61e0b74b19b043a19cee39b4d9d57
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-22 01:43:08 +0000

    Testing Wed Oct 22 2025 01:43:08 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/bouncy.py
+++ chapter3/ex04/student/bouncy.py
@@ -1,11 +1,13 @@
+# bouncy.py
+
 height = float(input("Enter the height from which the ball is dropped: "))
 bounciness = float(input("Enter the bounciness index of the ball: "))
 bounces = int(input("Enter the number of times the ball is allowed to continue bouncing: "))
 
 total_distance = height
+
 for i in range(bounces):
     height *= bounciness
     total_distance += 2 * height
 
 print("Total distance traveled is:", total_distance, "units.")
-
