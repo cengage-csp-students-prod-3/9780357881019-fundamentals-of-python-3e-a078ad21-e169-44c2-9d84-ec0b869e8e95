a039f9cb0486174cc62901eaf52d0b3aaa94256d
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-22 02:01:11 +0000

    Testing Wed Oct 22 2025 02:01:11 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex06/student/leibniz.py
+++ chapter3/ex06/student/leibniz.py
@@ -1,1 +1,13 @@
-# Write your program here
+
+iterations = int(input("Enter the number of iterations: "))
+
+pi_approx = 0.0
+sign = 1
+
+for i in range(iterations):
+    pi_approx += sign * (1 / (2 * i + 1))
+    sign *= -1
+
+pi_approx *= 4
+
+print("The approximation of pi is", pi_approx)
