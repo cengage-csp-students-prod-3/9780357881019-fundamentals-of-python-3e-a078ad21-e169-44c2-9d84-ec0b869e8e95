6967e8ec7efb4d56eaed975cf4d7d56409c7dbe1
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-22 01:25:25 +0000

    Testing Wed Oct 22 2025 01:25:25 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex02/student/right.py
+++ chapter3/ex02/student/right.py
@@ -1,2 +1,13 @@
-# Write your program here
-print('Hello')
+
+side1 = float(input("Enter the first side: "))
+side2 = float(input("Enter the second side: "))
+side3 = float(input("Enter the third side: "))
+
+
+a, b, c = sorted([side1, side2, side3])
+
+
+if abs(c**2 - (a**2 + b**2)) < 1e-6:
+    print("The triangle is a right triangle.")
+else:
+    print("The triangle is not a right triangle.")
