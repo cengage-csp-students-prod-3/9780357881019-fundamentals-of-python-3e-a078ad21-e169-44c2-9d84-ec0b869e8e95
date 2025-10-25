c655315af47f9fe2ac037e78936813f2efe6423b
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-25 15:18:50 +0000

    Testing Sat Oct 25 2025 15:18:50 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex07/student/salary.py
+++ chapter3/ex07/student/salary.py
@@ -1,1 +1,13 @@
-# Write your program here
+
+starting_salary = float(input("Enter the starting salary: $"))
+percent_increase = float(input("Enter the annual % increase: "))
+years = int(input("Enter the number of years: "))
+
+print("\nYear   Salary")
+print("-------------")
+
+salary = starting_salary
+
+for year in range(1, years + 1):
+    print(f"{year:2d}    {salary:.2f}")
+    salary *= (1 + percent_increase / 100)
