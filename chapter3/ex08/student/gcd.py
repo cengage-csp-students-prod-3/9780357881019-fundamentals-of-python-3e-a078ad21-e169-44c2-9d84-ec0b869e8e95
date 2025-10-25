a134cfa3782b2a22c6dbc0cc2a50513fd0c10946
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-25 15:34:22 +0000

    Testing Sat Oct 25 2025 15:34:22 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex08/student/gcd.py
+++ chapter3/ex08/student/gcd.py
@@ -1,1 +1,9 @@
-# Write your program here
+
+small = int(input("Enter the smaller number: "))
+large = int(input("Enter the larger number: "))
+
+while small != 0:
+    remainder = large % small
+    large, small = small, remainder
+
+print("\nThe greatest common divisor is", large)
