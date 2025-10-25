7a5a818a8cd767e17cdb8d336ed21f45af5fb754
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-25 17:07:11 +0000

    Testing Sat Oct 25 2025 17:07:11 GMT+0000 (Coordinated Universal Time)

--- chapter3/dex01/student/computeAverage.py
+++ chapter3/dex01/student/computeAverage.py
@@ -1,8 +1,9 @@
 count = 1
 total = 0
-while count < 10:
+while count <= 10:  # use <= to include 10th score
     score = int(input("Enter test score number " + str(count) + ": "))
     total = total + score
     count = count + 1
-average = total / count
+
+average = total / 10  # divide by 10 because we are collecting 10 scores
 print("The average test score is", average)
