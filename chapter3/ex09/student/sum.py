2a8c01bdb836ad8d432940c2d4dc45d42796f2af
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-10-25 15:40:51 +0000

    Testing Sat Oct 25 2025 15:40:51 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex09/student/sum.py
+++ chapter3/ex09/student/sum.py
@@ -1,1 +1,18 @@
-# Write your program here
+
+total = 0.0
+count = 0
+
+while True:
+    data = input("Enter a number or press Enter to quit: ")
+    if data == "":
+        break
+    number = float(data)
+    total += number
+    count += 1
+
+if count > 0:
+    average = total / count
+    print("\nThe sum is", total)
+    print("The average is", average)
+else:
+    print("\nNo numbers were entered.")
