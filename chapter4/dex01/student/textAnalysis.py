9b63588a8e0b90ea2dd7f5eaab64b53cdf31fd3d
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-15 05:02:36 +0000

    Testing Sat Nov 15 2025 05:02:36 GMT+0000 (Coordinated Universal Time)

--- chapter4/dex01/student/textAnalysis.py
+++ chapter4/dex01/student/textAnalysis.py
@@ -1,44 +1,19 @@
-"""
-Program: textanalysis.py
-Author: Ken
-Computes and displays the Flesch Index and the Grade
-Level Equivalent for the readability of a text file.
-"""
+def count_syllables(word):
+    word = word.lower()
+    vowels = "aeiou"
+    syllables = 0
+    prev_char_was_vowel = False
 
-# Take the inputs
-fileName = input("Enter the file name: ")
-inputFile = open(fileName, 'r')
-text = inputFile.read()
+    for char in word:
+        if char in vowels:
+            if not prev_char_was_vowel:  # ardışık ünlü değilse hece say
+                syllables += 1
+                prev_char_was_vowel = True
+        else:
+            prev_char_was_vowel = False
 
-# Count the sentences
-sentences = text.count('.') + text.count('?') + \
-            text.count(':') + text.count(';') + \
-            text.count('!')
+    # Kelime sessiz harfle bitiyorsa ve sadece 1 hece sayıldıysa minimum 1 hece
+    if syllables == 0:
+        syllables = 1
 
-# Count the words
-words = len(text.split())
-
-# Count the syllables
-syllables = 0
-vowels = "aeiouAEIOU"
-for word in text.split():
-    for vowel in vowels:
-        syllables += word.count(vowel)
-    for ending in ['es', 'ed', 'e']:
-        if word.endswith(ending):
-            syllables -= 1
-    if word.endswith('le'):
-        syllables += 1
-
-# Compute the Flesch Index and Grade Level
-index = 206.835 - 1.015 * (words / sentences) - \
-        84.6 * (syllables / words)
-level = int(round(0.39 * (words / sentences) + 11.8 * \
-                  (syllables / words) - 15.59))
-
-# Output the results
-print("The Flesch Index is", index)
-print("The Grade Level Equivalent is", level)
-print(sentences, "sentences")
-print(words, "words")
-print(syllables, "syllables")   
+    return syllables
