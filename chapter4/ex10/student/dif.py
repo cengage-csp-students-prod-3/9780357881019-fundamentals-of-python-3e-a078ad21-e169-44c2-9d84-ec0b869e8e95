75938b58be913a412d105cf90ca545900b495926
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-15 05:00:16 +0000

    Testing Sat Nov 15 2025 05:00:16 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex10/student/dif.py
+++ chapter4/ex10/student/dif.py
@@ -1,5 +1,4 @@
-# dif.py
-# Compares two text files line by line
+
 
 def main():
     file1_name = input("Enter the first file name: ")
@@ -13,15 +12,15 @@ def main():
             line1 = f1.readline()
             line2 = f2.readline()
 
-            # Her iki dosya da sona geldiyse döngüden çık
+           
             if not line1 and not line2:
                 break
 
-            # Satırlar farklıysa veya bir dosya bitti diğer bitmedi
+           
             if line1 != line2:
                 same = False
                 print("No")
-                # Sona gelmiş ama diğer dosya devam ediyorsa boş string yerine newline önle
+                
                 print(line1.rstrip("\n"))
                 print(line2.rstrip("\n"))
                 break
