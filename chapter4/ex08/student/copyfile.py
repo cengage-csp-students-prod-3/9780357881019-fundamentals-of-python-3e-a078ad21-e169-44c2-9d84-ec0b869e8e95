278c88bdb7dbaae44eb350636025a7316a083ddb
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-15 04:45:40 +0000

    Testing Sat Nov 15 2025 04:45:40 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex08/student/copyfile.py
+++ chapter4/ex08/student/copyfile.py
@@ -1,15 +1,12 @@
-# copyfile.py
-# Copies the contents of one text file to another
+
 
 def main():
     input_file = input("Enter the input file name: ")
     output_file = input("Enter the output file name: ")
 
-    # Girdi dosyasını oku
     with open(input_file, "r", encoding="utf-8") as f:
         content = f.read()
 
-    # Çıktı dosyasına yaz
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(content)
 
