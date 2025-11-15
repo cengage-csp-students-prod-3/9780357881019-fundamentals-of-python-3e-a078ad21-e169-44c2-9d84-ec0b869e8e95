4cb34cdf308300afa4c162377b83112aa00cf514
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-15 04:18:21 +0000

    Testing Sat Nov 15 2025 04:18:21 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex05/student/shiftleft.py
+++ chapter4/ex05/student/shiftleft.py
@@ -1,1 +1,14 @@
-# Write your program here
+
+def shift_left(bit_string):
+    if len(bit_string) == 0:
+        return bit_string
+    # sol bit string[0], kalan string[1:]
+    return bit_string[1:] + bit_string[0]
+
+def main():
+    bits = input("Enter a string of bits: ")
+    shifted = shift_left(bits)
+    print(shifted)
+
+if __name__ == "__main__":
+    main()
--- chapter4/ex05/student/shiftright.py
+++ chapter4/ex05/student/shiftright.py
@@ -1,1 +1,15 @@
-# Write your program here
+
+
+def shift_right(bit_string):
+    if len(bit_string) == 0:
+        return bit_string
+    # sağ bit string[-1], kalan string[:-1]
+    return bit_string[-1] + bit_string[:-1]
+
+def main():
+    bits = input("Enter a string of bits: ")
+    shifted = shift_right(bits)
+    print(shifted)
+
+if __name__ == "__main__":
+    main()
