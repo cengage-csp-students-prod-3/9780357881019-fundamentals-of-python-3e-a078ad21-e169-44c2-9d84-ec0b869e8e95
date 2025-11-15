002e7159db2e2c7db1ca724a654d8531494e80d4
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-15 04:01:20 +0000

    Testing Sat Nov 15 2025 04:01:20 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/decrypt.py
+++ chapter4/ex03/student/decrypt.py
@@ -1,1 +1,29 @@
-efg!dbftbs`fodszqu)ufyu-!tufq*;!!!!fodszqufe!>!##!!!!gps!dibs!jo!ufyu;!!!!!!!!fodszqufe!,>!dis))pse)dibs*!,!tufq*!&!367*!!!!!!sfuvso!fodszqufeefg!nbjo)*;!!!!joqvu`gjmf!>!joqvu)#Foufs!uif!joqvu!gjmf!obnf;!#*!!!!pvuqvu`gjmf!>!joqvu)#Foufs!uif!pvuqvu!gjmf!obnf;!#*!!!!tufq!>!jou)joqvu)#Foufs!uif!ejtubodf!wbmvf;!#**!!!!!!!xjui!pqfo)joqvu`gjmf-!#s#-!fodpejoh>#vug.9#*!bt!g;!!!!!!!!dpoufou!>!g/sfbe)*!!!!!!!!fodszqufe`dpoufou!>!dbftbs`fodszqu)dpoufou-!tufq*!!!!!!xjui!pqfo)pvuqvu`gjmf-!#x#-!fodpejoh>#vug.9#*!bt!g;!!!!!!!!g/xsjuf)fodszqufe`dpoufou*!!!!qsjou)#Gjmf!fodszqufe!tvddfttgvmmz"#*jg!``obnf``!>>!#``nbjo``#;!!!!nbjo)*
+# decrypt_file.py
+# Decrypts an entire text file encrypted with a Caesar cipher
+
+def caesar_decrypt(text, step):
+    decrypted = ""
+    for char in text:
+        decrypted += chr((ord(char) - step) % 256)  # tüm ASCII karakterleri kapsar
+    return decrypted
+
+def main():
+    input_file = input("Enter the input file name: ")
+    output_file = input("Enter the output file name: ")
+    step = int(input("Enter the distance value: "))
+
+    # Dosyayı oku
+    with open(input_file, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    # Şifre çöz
+    decrypted_content = caesar_decrypt(content, step)
+
+    # Çözülmüş içeriği yaz
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(decrypted_content)
+
+    print("File decrypted successfully!")
+
+if __name__ == "__main__":
+    main()
