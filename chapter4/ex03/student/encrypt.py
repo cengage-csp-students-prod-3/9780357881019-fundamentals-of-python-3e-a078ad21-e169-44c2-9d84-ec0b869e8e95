b6a637d31b4c777f7277a149f00129bca604ea8e
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-15 03:47:54 +0000

    Testing Sat Nov 15 2025 03:47:54 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/encrypt.py
+++ chapter4/ex03/student/encrypt.py
@@ -1,10 +1,8 @@
-# encrypt_file.py
-# Encrypts an entire text file using a Caesar cipher
 
 def caesar_encrypt(text, step):
     encrypted = ""
     for char in text:
-        encrypted += chr((ord(char) + step) % 256)  # tüm ASCII karakterleri kapsar
+        encrypted += chr((ord(char) + step) % 256)  
     return encrypted
 
 def main():
@@ -12,14 +10,14 @@ def main():
     output_file = input("Enter the output file name: ")
     step = int(input("Enter the distance value: "))
 
-    # Dosyayı oku
+   
     with open(input_file, "r", encoding="utf-8") as f:
         content = f.read()
 
-    # Şifrele
+    
     encrypted_content = caesar_encrypt(content, step)
 
-    # Şifrelenmiş içeriği yaz
+  
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(encrypted_content)
 
