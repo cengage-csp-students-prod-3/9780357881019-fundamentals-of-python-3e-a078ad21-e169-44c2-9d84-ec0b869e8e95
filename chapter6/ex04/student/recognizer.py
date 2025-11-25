7d85c7af462820cb12cc9c3ff66ec517ff5beba6
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-25 05:47:22 +0000

    Testing Tue Nov 25 2025 05:47:22 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex04/student/recognizer.py
+++ chapter6/ex04/student/recognizer.py
@@ -1,1 +1,22 @@
-# Write your code here
+def isSorted(lst):
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            return False
+    return True
+
+def main():
+    # Test some example lists
+    lists_to_test = [
+        [],
+        [1],
+        [1, 2, 3, 4],
+        [4, 3, 2, 1],
+        [1, 2, 2, 3],
+        [1, 3, 2, 4]
+    ]
+
+    for lst in lists_to_test:
+        print(f"List: {lst} -> Sorted? {isSorted(lst)}")
+
+if __name__ == "__main__":
+    main()
