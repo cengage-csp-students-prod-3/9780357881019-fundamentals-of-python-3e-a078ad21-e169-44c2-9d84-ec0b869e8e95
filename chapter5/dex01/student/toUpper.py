3f7054b2e7e4c57be066668d250c1e2f5d38e57b
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-25 04:03:48 +0000

    Testing Tue Nov 25 2025 04:03:47 GMT+0000 (Coordinated Universal Time)

--- chapter5/dex01/student/toUpper.py
+++ chapter5/dex01/student/toUpper.py
@@ -1,4 +1,15 @@
+"""
+Program: toUpper.py
+Author: Jack
+Converts all words in a list to uppercase.
+"""
+
+# Orijinal liste
 listOfWords = ["Apple", "orange", "banana"]
-for word in listOfWords:
-    word = word.upper()
+
+# Her kelimeyi büyük harfe çevir
+for i in range(len(listOfWords)):
+    listOfWords[i] = listOfWords[i].upper()
+
+# Sonucu yazdır
 print(listOfWords)
