4e767bc1b2b89f9bffe46dc3ffa366acfc20c5c5
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-25 05:40:16 +0000

    Testing Tue Nov 25 2025 05:40:16 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex03/student/testsort.py
+++ chapter6/ex03/student/testsort.py
@@ -1,1 +1,22 @@
-# Write your code here
+def isSorted(lst):
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            return False
+    return True
+
+def main():
+    # Test cases
+    test_lists = [
+        [],
+        [1],
+        [1, 2, 3, 4, 5],
+        [5, 4, 3, 2, 1],
+        [1, 2, 2, 3],
+        [1, 3, 2, 4]
+    ]
+    
+    for lst in test_lists:
+        print(f"List: {lst} -> Sorted? {isSorted(lst)}")
+
+if __name__ == "__main__":
+    main()
