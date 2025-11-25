34f22dd4d3aa5f723f1bd74207e31bbc2acf99a1
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-25 19:32:40 +0000

    Testing Tue Nov 25 2025 19:32:40 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex04/student/recognizer.py
+++ chapter6/ex04/student/recognizer.py
@@ -5,36 +5,26 @@ articles = {"the", "a", "an"}
 nouns = {"boy", "girl", "ball", "bat"}
 verbs = {"saw", "hit"}
 prepositions = {"with"}
-adjectives = {"red", "big", "small"}          # NEW
-conjunctions = {"and", "but"}                # NEW (you can extend)
+adjectives = {"red", "big", "small"}
+conjunctions = {"and"}
 
 # --- PARSER FUNCTIONS ---
 
 def nounPhrase(words, index):
-    """Noun phrase can be:
-       article + noun
-       article + adjective + noun
-       noun
-    """
     if index < len(words) and words[index] in articles:
         index += 1
-        # check adjective
         if index < len(words) and words[index] in adjectives:
             index += 1
-        # expect noun
         if index < len(words) and words[index] in nouns:
             return True, index + 1
-        else:
-            return False, index
+        return False, index
     else:
-        # Allow bare noun (optional if your instructor wants it)
         if index < len(words) and words[index] in nouns:
             return True, index + 1
         return False, index
 
 
 def prepPhrase(words, index):
-    """prepositional phrase: preposition + noun phrase"""
     if index < len(words) and words[index] in prepositions:
         ok, index = nounPhrase(words, index + 1)
         return ok, index
@@ -42,27 +32,19 @@ def prepPhrase(words, index):
 
 
 def verbPhrase(words, index):
-    """Verb phrase can be:
-       verb + noun phrase
-       verb + noun phrase + prepositional phrase
-    """
     if index < len(words) and words[index] in verbs:
         index += 1
         ok, index = nounPhrase(words, index)
         if not ok:
             return False, index
-
-        # optional prepositional phrase
         ok2, newIndex = prepPhrase(words, index)
         if ok2:
             return True, newIndex
         return True, index
-
     return False, index
 
 
 def simpleSentence(words, index):
-    """A simple sentence: noun phrase + verb phrase"""
     ok, index = nounPhrase(words, index)
     if not ok:
         return False, index
@@ -70,15 +52,10 @@ def simpleSentence(words, index):
 
 
 def sentence(words):
-    """A full sentence can be:
-       simpleSentence
-       simpleSentence + conjunction + simpleSentence
-    """
     ok, index = simpleSentence(words, 0)
     if not ok:
         return False
 
-    # Check if sentence continues with a conjunction
     if index < len(words) and words[index] in conjunctions:
         index += 1
         ok2, index = simpleSentence(words, index)
@@ -98,9 +75,9 @@ def main():
         words = user_input.lower().split()
 
         if sentence(words):
-            print("Ok, grammatically correct")
+            print("correct")
         else:
-            print("Not grammatically correct")
+            print("incorrect")   # REQUIRED FOR THE TESTS
 
 
 if __name__ == "__main__":
