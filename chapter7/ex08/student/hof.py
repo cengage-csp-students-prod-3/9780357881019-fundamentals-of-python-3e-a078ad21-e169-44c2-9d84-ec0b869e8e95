b37d770299e24a2826e28aba98f5f1c174d0789c
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-26 02:10:46 +0000

    Testing Wed Nov 26 2025 02:10:46 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex08/student/hof.py
+++ chapter7/ex08/student/hof.py
@@ -1,1 +1,26 @@
-# Write your code here
+# hof.py
+# Higher-order function implementations
+
+def myMap(func, lst):
+    """Apply func to each element of lst and return a new list."""
+    result = []
+    for item in lst:
+        result.append(func(item))
+    return result
+
+def myFilter(func, lst):
+    """Return a list of elements in lst for which func(item) is True."""
+    result = []
+    for item in lst:
+        if func(item):
+            result.append(item)
+    return result
+
+def myReduce(func, lst):
+    """Apply func cumulatively to the items of lst, from left to right, to reduce to a single value."""
+    if not lst:
+        raise ValueError("myReduce() of empty sequence with no initial value")
+    result = lst[0]
+    for item in lst[1:]:
+        result = func(result, item)
+    return result
--- chapter7/ex08/student/testhof.py
+++ chapter7/ex08/student/testhof.py
@@ -1,1 +1,36 @@
-# Write your code here
+# testhof.py
+from hof import myMap, myFilter, myReduce
+from functools import reduce  # Python's built-in reduce
+
+# Test data
+test_lists = [
+    [0, 1, 2, 3, 4],
+    [3],
+    []
+]
+
+def square(x):
+    return x ** 2
+
+def is_odd(x):
+    return x % 2 == 1
+
+def add(x, y):
+    return x + y
+
+for lst in test_lists:
+    print(f"Argument list:   {lst}")
+    
+    # Test map
+    print("map with ** 2  :", list(map(square, lst)))
+    print("myMap with ** 2:", myMap(square, lst))
+    
+    # Test filter
+    print("filter with odd  :", list(filter(is_odd, lst)))
+    print("myFilter with odd:", myFilter(is_odd, lst))
+    
+    # Test reduce (handle empty list safely)
+    if lst:
+        print("reduce with +   :", reduce(add, lst))
+        print("myReduce with + :", myReduce(add, lst))
+    print()
