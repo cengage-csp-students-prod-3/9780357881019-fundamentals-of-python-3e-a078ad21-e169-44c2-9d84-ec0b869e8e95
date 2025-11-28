7083d27222320fec6bb2ab66c2ee77680d4d9c30
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-28 04:48:32 +0000

    Testing Fri Nov 28 2025 04:48:32 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex05/student/posterize.py
+++ chapter8/ex05/student/posterize.py
@@ -1,1 +1,26 @@
-# Write your code here
+from images import Image
+
+def posterize(image, color):
+    w = image.getWidth()
+    h = image.getHeight()
+    for x in range(w):
+        for y in range(h):
+            r, g, b = image.getPixel(x, y)
+            # Ortalama 128 eşik değeri: üstü seçilen renk, altı beyaz
+            if (r + g + b) / 3 >= 128:
+                image.setPixel(x, y, color)
+            else:
+                image.setPixel(x, y, (255, 255, 255))
+
+def main():
+    filename = input("Enter the image file name: ")
+    r = int(input("Enter an integer [0..255] for red: "))
+    g = int(input("Enter an integer [0..255] for green: "))
+    b = int(input("Enter an integer [0..255] for blue: "))
+
+    img = Image(filename)
+    posterize(img, (r, g, b))
+    img.draw()
+
+main()
+
