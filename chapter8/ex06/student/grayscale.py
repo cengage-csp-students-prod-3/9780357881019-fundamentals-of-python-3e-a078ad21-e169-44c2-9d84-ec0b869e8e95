544b0a4324dc5b54aaed7d2b228cfac36a3069a3
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-28 04:54:49 +0000

    Testing Fri Nov 28 2025 04:54:49 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex06/student/grayscale.py
+++ chapter8/ex06/student/grayscale.py
@@ -1,1 +1,19 @@
 # Write your code here
+from images import Image
+
+def grayscale(image):
+    w = image.getWidth()
+    h = image.getHeight()
+    for x in range(w):
+        for y in range(h):
+            r, g, b = image.getPixel(x, y)
+            gray = int((r + g + b) / 3)  # basit ortalama
+            image.setPixel(x, y, (gray, gray, gray))
+
+def main():
+    filename = input("Enter the image file name: ")
+    img = Image(filename)
+    grayscale(img)
+    img.draw()
+
+main()
