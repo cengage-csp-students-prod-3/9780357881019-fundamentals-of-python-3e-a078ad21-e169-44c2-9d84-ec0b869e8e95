415a381e72e1eff124caed218dea3fd2c7ecafcc
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-11-28 04:33:15 +0000

    Testing Fri Nov 28 2025 04:33:15 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex07/student/invert.py
+++ chapter8/ex07/student/invert.py
@@ -1,1 +1,17 @@
-# Write your code here
+from images import Image
+
+def invert(image):
+    w = image.getWidth()
+    h = image.getHeight()
+    for x in range(w):
+        for y in range(h):
+            r, g, b = image.getPixel(x, y)
+            image.setPixel(x, y, (255 - r, 255 - g, 255 - b))
+
+def main():
+    filename = input("Enter the image file name: ")
+    img = Image(filename)
+    invert(img)
+    img.draw()
+
+main()
