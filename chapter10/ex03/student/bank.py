0e40a7edf9ebc9fe42174c1ff0cddf0b635f0c87
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-12-08 00:47:22 +0000

    Testing Mon Dec 08 2025 00:47:22 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex03/student/bank.py
+++ chapter10/ex03/student/bank.py
@@ -1,29 +1,15 @@
-"""
-File: bank.py
-Programming Exercise 10.3
-The str for Bank returns a string of accounts sorted by name.
-"""
 import pickle
 import random
 from savingsaccount import SavingsAccount
 
 class Bank:
-    """This class represents a bank as a collection of savings accounts.
-    An optional file name is also associated
-    with the bank, to allow transfer of accounts to and
-    from permanent file storage."""
+    """This class represents a bank as a collection of savings accounts."""
 
-    # The state of the bank is a dictionary of accounts and
-    # a file name.  If the file name is None, a file name
-    # for the bank has not yet been established.
-
-    def __init__(self, fileName = None):
-        """Creates a new dictionary to hold the accounts.
-        If a file name is provided, loads the accounts from
-        a file of pickled accounts."""
+    def __init__(self, fileName=None):
+        """Creates a new dictionary to hold the accounts."""
         self.accounts = {}
         self.fileName = fileName
-        if fileName != None:
+        if fileName is not None:
             fileObj = open(fileName, 'rb')
             while True:
                 try:
@@ -34,8 +20,10 @@ class Bank:
                     break
 
     def __str__(self):
-        """Returns the string representation of the bank."""
-        return "\n".join(map(str, self.accounts.values()))
+        """Returns the string representation of the bank with accounts sorted by name."""
+        # accounts.values() listesini al, SavingsAccount sınıfındaki __lt__ metoduna göre sırala
+        sorted_accounts = sorted(self.accounts.values())
+        return "\n".join(map(str, sorted_accounts))
 
     def makeKey(self, name, pin):
         """Returns a key for the account."""
@@ -47,48 +35,40 @@ class Bank:
         self.accounts[key] = account
 
     def remove(self, name, pin):
-        """Removes the account from the bank and
-        and returns it, or None if the account does
-        not exist."""
+        """Removes the account from the bank and returns it, or None if the account does not exist."""
         key = self.makeKey(name, pin)
         return self.accounts.pop(key, None)
 
     def get(self, name, pin):
-        """Returns the account from the bank,
-        or returns None if the account does
-        not exist."""
+        """Returns the account from the bank, or returns None if the account does not exist."""
         key = self.makeKey(name, pin)
         return self.accounts.get(key, None)
 
     def computeInterest(self):
-        """Computes and returns the interest on
-        all accounts."""
+        """Computes and returns the interest on all accounts."""
         total = 0
-        for account in self._accounts.values():
+        for account in self.accounts.values():
             total += account.computeInterest()
         return total
 
     def getKeys(self):
         """Returns a sorted list of keys."""
-        # Exercise
-        return []
+        return sorted(self.accounts.keys())
 
-    def save(self, fileName = None):
-        """Saves pickled accounts to a file.  The parameter
-        allows the user to change file names."""
-        if fileName != None:
+    def save(self, fileName=None):
+        """Saves pickled accounts to a file."""
+        if fileName is not None:
             self.fileName = fileName
-        elif self.fileName == None:
+        elif self.fileName is None:
             return
         fileObj = open(self.fileName, 'wb')
         for account in self.accounts.values():
             pickle.dump(account, fileObj)
         fileObj.close()
 
+
 # Functions for testing
-def createBank(numAccounts = 1):
-    """Returns a new bank with the given number of 
-    accounts."""
+def createBank(numAccounts=1):
     names = ("Brandon", "Molly", "Elena", "Mark", "Tricia",
              "Ken", "Jill", "Jack")
     bank = Bank()
@@ -99,9 +79,7 @@ def createBank(numAccounts = 1):
         bank.add(SavingsAccount(name, str(pinNumber), balance))
     return bank
 
-def testBank(number = 0):
-    """Returns a bank with the specified number of accounts and/or
-    the accounts loaded from the specified file name."""
+def testBank(number=0):
     bank = Bank()
     for i in range(number):
         bank.add(SavingsAccount('Name' + str(i + 1),
@@ -109,10 +87,7 @@ def testBank(number = 0):
                                 100.00))
     return bank
 
-def main(number = 10, fileName = None):
-    """Creates and prints a bank, either from
-    the optional file name argument or from the optional
-    number."""
+def main(number=10, fileName=None):
     bank = testBank(9)
     print(bank)
 
