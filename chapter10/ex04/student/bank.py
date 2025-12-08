40d55ee52ac014ca9fb79f96464335a3b348eb1f
ZiynetAkcaComputerScience1 <ziynet.akca@student.northampton.edu>
2025-12-08 01:11:18 +0000

    Testing Mon Dec 08 2025 01:11:18 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex04/student/bank.py
+++ chapter10/ex04/student/bank.py
@@ -1,129 +1,34 @@
-"""
-File: bank.py
-This module defines the Bank class.
-"""
-import pickle
-import random
-from savingsaccount import SavingsAccount
+from breezypythongui import EasyFrame
+from tkinter import messagebox
 
-class Bank:
-    """This class represents a bank as a collection of savnings accounts.
-    An optional file name is also associated
-    with the bank, to allow transfer of accounts to and
-    from permanent file storage."""
+class ATMView(EasyFrame):
+    def __init__(self, model):
+        EasyFrame.__init__(self, title="ATM")
+        self.model = model
+        self.failures = 0  # Başarısız giriş sayacı
 
-    # The state of the bank is a dictionary of accounts and
-    # a file name.  If the file name is None, a file name
-    # for the bank has not yet been established.
+        # GUI bileşenleri
+        self.addLabel("Name", row=0, column=0)
+        self.nameField = self.addTextField("", row=0, column=1)
 
-    def __init__(self, fileName = None):
-        """Creates a new dictionary to hold the accounts.
-        If a file name is provided, loads the accounts from
-        a file of pickled accounts."""
-        self.accounts = {}
-        self.fileName = fileName
-        if fileName != None:
-            fileObj = open(fileName, "rb")
-            while True:
-                try:
-                    account = pickle.load(fileObj)
-                    self.add(account)
-                except EOFError:
-                    fileObj.close()
-                    break
+        self.addLabel("PIN", row=1, column=0)
+        self.pinField = self.addTextField("", row=1, column=1)
 
-    def __str__(self):
-        """Returns the string representation of the bank."""
-        return "\n".join(map(str, self.accounts.values()))
+        self.statusLabel = self.addLabel("Status", row=2, column=0, columnspan=2)
 
-    def makeKey(self, name, pin):
-        """Makes and returns a key from name and pin."""
-        return name + "/" + pin
+        # Login butonu
+        self.loginButton = self.addButton(text="Login", row=3, column=0, columnspan=2, command=self.login)
 
-    def add(self, account):
-        """Inserts an account with name and pin as a key."""
-        key = self.makeKey(account.getName(), account.getPin())
-        self.accounts[key] = account
+    def login(self):
+        name = self.nameField.getText()
+        pin = self.pinField.getText()
 
-    def remove(self, name, pin):
-        """Removes the account from the bank and
-        and returns it, or None if the account does
-        not exist."""
-        key = self.makeKey(name, pin)
-        return self.accounts.pop(key, None)
-
-    def get(self, name, pin):
-        """Returns the account from the bank,
-        or returns None if the account does
-        not exist."""
-        key = self.makeKey(name, pin)
-        return self.accounts.get(key, None)
-
-    def computeInterest(self):
-        """Computes and returns the interest on
-        all accounts."""
-        total = 0
-        for account in self._accounts.values():
-            total += account.computeInterest()
-        return total
-
-    def getKeys(self):
-        """Returns a sorted list of keys."""
-        # Exercise
-        return []
-
-    def save(self, fileName = None):
-        """Saves pickled accounts to a file.  The parameter
-        allows the user to change file names."""
-        if fileName != None:
-            self.fileName = fileName
-        elif self.fileName == None:
-            return
-        fileObj = open(self.fileName, "wb")
-        for account in self.accounts.values():
-            pickle.dump(account, fileObj)
-        fileObj.close()
-
-# Functions for testing
-       
-def createBank(numAccounts = 1):
-    """Returns a new bank with the given number of 
-    accounts."""
-    names = ("Brandon", "Molly", "Elena", "Mark", "Tricia",
-             "Ken", "Jill", "Jack")
-    bank = Bank()
-    upperPin = numAccounts + 1000
-    for pinNumber in range(1000, upperPin):
-        name = random.choice(names)
-        balance = float(random.randint(100, 1000))
-        bank.add(SavingsAccount(name, str(pinNumber), balance))
-    return bank
-
-def testAccount():
-    """Test function for savings account."""
-    account = SavingsAccount("Ken", "1000", 500.00)
-    print(account)
-    print(account.deposit(100))
-    print("Expect 600:", account.getBalance())
-    print(account.deposit(-50))
-    print("Expect 600:", account.getBalance())
-    print(account.withdraw(100))
-    print("Expect 500:", account.getBalance())
-    print(account.withdraw(-50))
-    print("Expect 500:", account.getBalance())
-    print(account.withdraw(100000))
-    print("Expect 500:", account.getBalance())
-
-def main(number = 10, fileName = None):
-    """Creates and prints a bank, either from
-    the optional file name argument or from the optional
-    number."""
-    testAccount()
-    if fileName:
-        bank = Bank(fileName)
-    else:
-        bank = createBank(number)
-    print(bank)
-
-if __name__ == "__main__":
-    main()
+        if self.model.checkLogin(name, pin):
+            self.statusLabel["text"] = "Login successful!"
+            self.failures = 0  # Başarılı girişte sayaç sıfırlanır
+        else:
+            self.failures += 1
+            self.statusLabel["text"] = f"Login failed ({self.failures} of 3)"
+            if self.failures >= 3:
+                messagebox.showwarning("Alert", "Three successive login failures! The police will be called.")
+                self.loginButton["state"] = "disabled"  # Butonu devre dışı bırak
